a7ac01445144e2632ca0b960a7ab712d052f1afc
zerosp3c <patrickmbownes@gmail.com>
2017-06-16 00:13:55 -0400

    update ship.js

--- js/ship.js
+++ js/ship.js
@@ -1,37 +1,113 @@
+
+
 /*
   This is the module that creates the ship. 
 */
 
 var Ship = {
+    //global variables
     MAXIMUM_SPEED: 800,
     WEIGHT_PER_ENGINE: 5,
+    SPEED_PER_ENGINE: 20,
     CREW_WEIGHT: 2,
     DRONE_WEIGHT: 0.33,
     LOOT_WEIGHT: 0.5,
     GUNS_WEIGHT: 0.8,
     FOOD_WEIGHT: 0.2,
+    FUEL_WEIGHT: 0.3,
     CREW_HUNGER: 0.2,
     DAY_PER_DISTANCE: 0.33,
+    FUEL_PER_DAY: 1,
     EVENT_PROBABILITY: 0.15,
 
-    init: function(stats) {
-		this.day = stats;
-        this.distance = stats.distance;
-        this.crew = stats.crew;
-        this.food = stats.food;
-        this.engines = stats.engines;
-        this.cargo = stats.cargo;
-        this.money = stats.money;
-        this.guns = stats.guns;
-        this.fuel = stats.fuel;
-        this.loot = stats.loot;
-		}
+    // variables
+    cargo: [],
+    cargoWeight: 0,
+    armory: [],
+    damage: 0,
+    shipWeight: 0,
+    
+    // init and initiate with standard player or enemy settings
+    init: function(distance,crew,food,engines,cargo,money,guns,drones,fuel,loot) {
+        this.day = 0;
+        this.armor = 200;
+        this.damage =  0;
+        this.distance = distance;
+        this.crew = crew;
+        this.food = food;
+        this.engines = engines;
+        this.cargoCapacity = cargo;
+        this.money = money;
+        this.guns = guns;
+        this.drones = drones;
+        this.fuel = fuel;
+        this.loot = loot;
+		},
+
+    // player initiates
+    initiate: function() {
+      this.init(0,2,10,6,10,0,1,0,10,0);
+    },
+
+    // shhip functions
+    weighShip: function() {
+        var crew = this.crew * Ship.CREW_WEIGHT;
+        var food = this.food * Ship.FOOD_WEIGHT;
+        var guns = this.guns * Ship.GUNS_WEIGHT;
+        var fuel = this.fuel * Ship.FUEL_WEIGHT;
+        var loot = this.loot * Ship.LOOT_WEIGHT;
+        var shipWeight = crew + food + guns + fuel + loot;
+        var weightCapacity = this.engines * Ship.WEIGHT_PER_ENGINE;
+        this.shipWeight = shipWeight;
+        if (shipWeight <= weightCapacity) {
+            return true;
+        } else {
+            return false;
+        };
+    },
+
+    attemptWeight: function(weight) {
+        this.weighShip();
+        attempt = this.shipWeight + weight;
+        capacity = this.engines * Ship.WEIGHT_PER_ENGINE;
+        if (attempt <= capacity) {
+            return true;
+        } else {
+            return false;
+        };
+    },
+
+    addCargo: function(item) {
+        var name = item[0];
+        var description = item[1];
+        var type = item[2];
+        var value = item[3];
+        var weight = item[4];
+        var attempt = this.attemptWeight(weight);
+        if (attempt === true) {
+            this.cargo.push(item);
+            this.shipWeight += weight;
+        } else {
+            console.log('cannot add item')
+            return false;
+        }
+    },
+    
+    addWeapon: function(weapon) {
+        var name = weapon[0];
+        var damage = weapon[1];
+        var flavor = weapon[2];
+        var value = weapon[3];
+        var weight = weapon[4];
+        var attempt = this.attemptWeight(weight);
+        if (attempt === true) {
+            this.armory.push(weapon)
+            this.damage += damage;
+            this.shipWeight += weight;
+            console.log(this.damage);
+        } else {
+            console.log('cannot add weapon');
+            return false;
+        };
+    },
 };
-
-
-/*
-  Test
-*/
-
-userShip = Ship.init(0,0,1,80,2,200,2,80,0);
-console.log(userShip)
--- js/ui.js
+++ js/ui.js
@@ -1,123 +1,121 @@
-// create name space for BlueD
-var BlueD = BlueD ||{};
+/*
+    This module creates the UI
+*/
 
-// create BlueD.UI
-var BlueD = {
-    UI: {
-        userTerminalText: '',
-        log: [],
+var UI = {
+    userTerminalText: '',
+    log: [],
+    inbox: [],
 
-        // refresh clickers
-        refreshClickListener: function() {
-            // turn off button listeners
-            $('#wrapper .close-message').off();
-            $('#wrapper #user-log').off();
-            $('#wrapper #log-close').off();
-            $('#wrapper .user-terminal-text').off();
+    // refresh clickers
+    refreshClickListener: function() {
+         // turn off button listeners
+        $('#wrapper .close-message').off();
+        $('#wrapper #user-log').off();
+        $('#wrapper #log-close').off();
+        $('#wrapper #user-terminal-text').off();
 
-            // turn on buttons
-            $('#wrapper .user-terminal-text').on('keydown', function(event) {
-                var length = $('.user-terminal-text').val().length;
-                if (event.which === 13) {
-                    event.preventDefault();
-                    BlueD.UI.log.push([$('.user-terminal-text').val(),'input']);
-                    BlueD.UI.userTerminalText = $('.user-terminal-text').val().slice(15);
-                    $echo = "<p>>> " + BlueD.UI.userTerminalText + "</p>";
-                    BlueD.UI.echo($echo);
-                } else if (event.which === 8 && length <= 15) {
-                    event.preventDefault();
-                } else if (event.which === 38) {
-                    event.preventDefault();
-                    $echo = BlueD.UI.userTerminalText;
-                    BlueD.UI.reverseEcho($echo);
-                };
-            });
-
-            $('#wrapper .close-message').on('click', function() {
-                var messageId = 'msg' + $(this).attr('id').slice(3);
-                var targetId = '#' + messageId;
-                $(targetId).remove();
-                BlueD.UI.inboxDelete(messageId);
-            });
-
-            $('#wrapper #user-log').on('click', function() {
-                $('#log').remove();
-                BlueD.UI.logRead();
-            });
-
-            $('#wrapper #log-close').on('click', function() {
-                $('#log').remove();
-            });
-        },
-
-        // work with terminal text
-        echo: function(echo) {
-            $('.user-terminal-text').val('dnl@olivaw: >> ');
-            $('#terminal-echo').empty();
-            $('#terminal-echo').append(echo);
-            BlueD.UI.refreshClickListener();
-        },
-
-        reverseEcho: function(echo) {
-            $('.user-terminal-text').val('');
-            $('.user-terminal-text').val('dnl@olivaw: >> ' + echo);
-            BlueD.UI.refreshClickListener();
-        },
-
-        // create and manage inbox
-        inbox: [],
-        inboxDelete: function(message) {
-            for (i in BlueD.UI.inbox) {
-                if (BlueD.UI.inbox[i] === message) {
-                    BlueD.UI.inbox.splice(i,1);
-                };
+        // turn on buttons
+        $('#wrapper #user-terminal-text').on('keydown', function(event) {
+            var length = $('#user-terminal-text').val().length;
+            if (event.which === 13) {
+                event.preventDefault();
+                UI.log.push([$('#user-terminal-text').val(),'input']);
+                UI.userTerminalText = $('#user-terminal-text').val().slice(15);
+                $echo = "<p>>> " + UI.userTerminalText + "</p>";
+                UI.echo($echo);
+            } else if (event.which === 8 && length <= 15) {
+                event.preventDefault();
+            } else if (event.which === 38) {
+                event.preventDefault();
+                $echo = UI.userTerminalText;
+                UI.reverseEcho($echo);
             };
-        },
+        });
 
-        // create and manage log
-        logRead: function() {
-            var $divLog = "<div class='row' id='log'><div class='col-sm-2' id='log-title'><h4>log</h4></div>\
-            <div class='col-sm-6'><ul id='log-ul'></ul></div>\
-            <div class='col-sm-2'><div id='log-close'><h4>close</h4></div></div>";
-            $('.screen').prepend($divLog);
-            for(i=0;i < BlueD.UI.log.length;i++) {
-                var $logEntry = "<li>" + BlueD.UI.log[i][0] + " - " + BlueD.UI.log[i][1] + "</li>";
-                $('#log-ul').prepend($logEntry);
-            };
-            BlueD.UI.refreshClickListener();
-        },
+        $('#wrapper .close-message').on('click', function() {
+            var messageId = 'msg' + $(this).attr('id').slice(3);
+            var targetId = '#' + messageId;
+            $(targetId).remove();
+            UI.inboxDelete(messageId);
+        });
 
-        // this function creates a message and adds it to inbox
-        createMessage: function(title,message,type,log) {
-            BlueD.UI.TOTAL_MESSAGES++;
-            var messageID = 'msg' + BlueD.UI.TOTAL_MESSAGES;
-            var buttonID = 'btn' + BlueD.UI.TOTAL_MESSAGES;
-            var $divMessageTitle = "<div class='message-title'>" + title + "</div>";
-            var $divCloseMessage = "<div class='btn btn-secondary close-message' role='button' id=" + buttonID +"></div>";
-            var $divMessageHeader = "<div class='message-header'>" + $divMessageTitle + $divCloseMessage + "</div>";
-            var $divMessageContent = "<div class='message-content'>" + message + "</div>";
-            var $divMessagePositive = "<div class='message-positive message' id=" + messageID + ">\
-            " + $divMessageHeader + $divMessageContent + "</div>";
-            var $divMessageNegative = "<div class='message-negative message' id=" + messageID + ">\
-            " + $divMessageHeader + $divMessageContent + "</div>";
-            switch(type) {
-                case 'positive':
-                    $('.screen').prepend($divMessagePositive);
-                    BlueD.UI.inbox.push(messageID);
-                    BlueD.UI.log.push([log,type]);
-                    break;
-                case 'negative':
-                    $('.screen').prepend($divMessageNegative);
-                    BlueD.UI.inbox.push(messageID);
-                    BlueD.UI.log.push([log,type]);
-                    break;
+        $('#wrapper #user-log').on('click', function() {
+            $('#log').remove();
+            UI.logRead();
+        });
+
+        $('#wrapper #log-close').on('click', function() {
+             $('#log').remove();
+        });
+    },
+
+    // work with terminal text
+    echo: function(echo) {
+        $('#user-terminal-text').val('dnl@olivaw: >> ');
+        $('#terminal-echo').empty();
+        $('#terminal-echo').append(echo);
+        UI.refreshClickListener();
+    },
+
+    reverseEcho: function(echo) {
+        $('#user-terminal-text').val('');
+        $('#user-terminal-text').val('dnl@olivaw: >> ' + echo);
+        UI.refreshClickListener();
+    },
+
+    // create and manage inbox
+    inboxDelete: function(message) {
+        for (i in UI.inbox) {
+            if (UI.inbox[i] === message) {
+                UI.inbox.splice(i,1);
             };
-            BlueD.UI.refreshClickListener();
-        },
+        };
+    },
+
+    // create and manage log
+    logRead: function() {
+        var $divLog = "<div class='row' id='log'><div class='col-sm-2' id='log-title'><h4>log</h4></div>\
+        <div class='col-sm-6'><ul id='log-ul'></ul></div>\
+        <div class='col-sm-2'><div id='log-close'><h4>close</h4></div></div>";
+        $('.screen').prepend($divLog);
+        for(i=0;i < UI.log.length;i++) {
+            var $logEntry = "<li>" + UI.log[i][0] + " - " + UI.log[i][1] + "</li>";
+            $('#log-ul').prepend($logEntry);
+        };
+        UI.refreshClickListener();
+    },
+
+    // this function creates a message and adds it to inbox
+    createMessage: function(title,message,type,log) {
+        UI.TOTAL_MESSAGES++;
+        var messageID = 'msg' + UI.TOTAL_MESSAGES;
+        var buttonID = 'btn' + UI.TOTAL_MESSAGES;
+        var $divMessageTitle = "<div class='message-title'>" + title + "</div>";
+        var $divCloseMessage = "<div class='btn btn-secondary close-message' role='button' id=" + buttonID +"></div>";
+        var $divMessageHeader = "<div class='message-header'>" + $divMessageTitle + $divCloseMessage + "</div>";
+        var $divMessageContent = "<div class='message-content'>" + message + "</div>";
+        var $divMessagePositive = "<div class='message-positive message' id=" + messageID + ">\
+        " + $divMessageHeader + $divMessageContent + "</div>";
+        var $divMessageNegative = "<div class='message-negative message' id=" + messageID + ">\
+        " + $divMessageHeader + $divMessageContent + "</div>";
+        switch(type) {
+            case 'positive':
+                $('.screen').prepend($divMessagePositive);
+                UI.inbox.push(messageID);
+                UI.log.push([log,type]);
+                break;
+            case 'negative':
+                $('.screen').prepend($divMessageNegative);
+                UI.inbox.push(messageID);
+                UI.log.push([log,type]);
+                break;
+        };
+        UI.refreshClickListener();
     },
 };
 
 // this code runs when the HTML DOM is ready
 $(document).ready(function() {
-    BlueD.UI.refreshClickListener();
+    UI.refreshClickListener();
 });
